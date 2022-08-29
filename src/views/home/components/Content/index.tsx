f97afdbc4e3c8e9ee922539f607eb17a5a53c7ee
huisir <273250950@qq.com>
2022-08-29 18:31:48 +0800

    style: 样式调整

--- src/views/home/components/Content/index.tsx
+++ src/views/home/components/Content/index.tsx
@@ -2,7 +2,7 @@
  * @Description: 内容
  * @Autor: HuiSir<github.com/huisir001>
  * @Date: 2022-08-29 11:10:44
- * @LastEditTime: 2022-08-29 17:36:50
+ * @LastEditTime: 2022-08-29 18:09:45
  */
 import { useState } from 'react'
 import './index.scss'
@@ -11,6 +11,14 @@ export default () => {
   const [searchVal, setSearchVal] = useState('')
   const handleSearch = (type: 'baidu' | 'google') => {
     console.log(type);
+    switch (type) {
+      case 'baidu':
+        window.open('//www.baidu.com/s?wd=' + searchVal)
+        break;
+      case 'google':
+        window.open('//www.google.com/search?q=' + searchVal)
+        break;
+    }
   }
   return (
     <section className="container">
