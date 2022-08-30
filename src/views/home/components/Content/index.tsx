466c069f9e56236bbcc7f75180c173d898338a01
HuiSir <273250950@qq.com>
2022-08-30 16:31:01 +0800

    feat: 完善

--- src/views/home/components/Content/index.tsx
+++ src/views/home/components/Content/index.tsx
@@ -2,15 +2,23 @@
  * @Description: 内容
  * @Autor: HuiSir<github.com/huisir001>
  * @Date: 2022-08-29 11:10:44
- * @LastEditTime: 2022-08-30 13:44:36
+ * @LastEditTime: 2022-08-30 16:28:55
  */
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { getBaiduKwds } from '@/api'
 import './index.scss'
 
 export default ({ bindFocus }: any) => {
+  // 输入框ref
+  const textInput = useRef(null)
+  // 输入框值
   const [searchVal, setSearchVal] = useState('')
+  // 备份输入值
+  const [tmpSearchVal, setTmpSearchVal] = useState('')
+  // 搜索建议数组
   const [kwds, setKwds] = useState([])
+  // 搜索建议选中下标
+  const [actSugIndex, setActSugIndex] = useState(-1)
 
   const queryBaiduKwds = async (wd: string) => {
     if (wd.trim() == '') {
@@ -35,14 +43,18 @@ export default ({ bindFocus }: any) => {
 
   const handleSearch = (type: 'baidu' | 'google', wd?: string) => {
     const val = wd ? encodeURIComponent(wd) : encodeURIComponent(searchVal)
+    if (val.trim() == '') {
+      return
+    }
     switch (type) {
       case 'baidu':
-        // window.open('//www.baidu.com/s?wd=' + val)
-        break;
+        window.open('//www.baidu.com/s?wd=' + val)
+        break
       case 'google':
         window.open('//www.google.com/search?q=' + val)
-        break;
-    }
+        break
+    };
+    (textInput as any).current.focus()
   }
 
   const handleSugClick = (wd: string) => {
@@ -50,14 +62,44 @@ export default ({ bindFocus }: any) => {
     handleSearch('baidu', wd)
   }
 
+  const handleInputKeyDown = (e: any) => {
+    if (e.key === 'Enter') {
+      handleSearch('baidu')
+    } else if (e.key === 'ArrowDown' && actSugIndex < kwds.length - 1) {
+      setActSugIndex((prev) => {
+        if (prev === -1) {
+          setTmpSearchVal(searchVal)
+        }
+        const currVal = prev + 1
+        setSearchVal(kwds[currVal])
+        return currVal
+      })
+    } else if (e.key === 'ArrowUp' && actSugIndex > -1) {
+      setActSugIndex((prev) => {
+        const currVal = prev - 1
+        if (currVal === -1) {
+          setSearchVal(tmpSearchVal)
+        } else {
+          setSearchVal(kwds[currVal])
+        }
+        return currVal
+      })
+    }
+    // 阻止默认事件
+    if (e.key === 'ArrowDown' || e.key === 'ArrowUp') {
+      e.preventDefault()
+    }
+  }
+
   return (
     <section className="container">
       <div className="search">
         <input type="text"
           className="search-input"
+          ref={textInput}
           value={searchVal}
           onChange={handleChange}
-          onKeyDown={({ key }) => key === 'Enter' && handleSearch('baidu')}
+          onKeyDown={handleInputKeyDown}
           onFocus={() => bindFocus(true)}
           onBlur={() => bindFocus(false)}
           placeholder="请输入关键字..."
@@ -70,7 +112,7 @@ export default ({ bindFocus }: any) => {
         {
           kwds.length > 0 && (
             <div className="search-suggest">
-              {kwds.map(w => (<div className='item' key={w} onClick={() => handleSugClick(w)}>{w}</div>))}
+              {kwds.map((w, i) => (<div className={`item${actSugIndex == i ? ' act' : ''}`} key={w} onClick={() => handleSugClick(w)}>{w}</div>))}
               <div className='intro'>搜索建议来自百度</div>
             </div>
           )
